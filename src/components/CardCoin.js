39b1a724d7636c2699d819dca2c815e667259fa0
ALXSMORA\Alexis Mora <alexis.mora1@icloud.com>
2021-02-02 22:30:51 -0600

    Add a new function Add the function fetchData to invoke from componentDidMount and can be call from a onclick function. Add a constructor for props and contain the state object. Add market rank in card token.

--- src/components/CardCoin.js
+++ src/components/CardCoin.js
@@ -2,31 +2,41 @@ import React, { Component } from "react";
 
 import axios from "axios";
 import { Card, CardBody } from "shards-react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+    faAngleDoubleUp,
+    faAngleDoubleDown,
+    faSync,
+} from "@fortawesome/free-solid-svg-icons";
 
 class CardCoin extends Component {
-    state = {
-        name: "",
-        price_usd: 0.0,
-        price_mxn: 0.0,
-        market_cap_usd: 0.0,
-        image: "",
-        price_change_24h: 0.0,
-        low_24h: 0.0,
-        high_24h: 0.0,
-    };
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: "TOKEN",
+            price_usd: 0.0,
+            price_mxn: 0.0,
+            market_cap_usd: 0.0,
+            market_cap_rank: 0,
+            image: "",
+            price_change_24h: 0.0,
+            low_24h: 0.0,
+            high_24h: 0.0,
+        };
+    }
 
-    componentDidMount() {
-        axios
+    async fetchData() {
+        await axios
             .get("https://api.coingecko.com/api/v3/coins/" + this.props.coin)
             .then((res) => {
                 const data = res.data;
-                console.log(data["market_data"]["price_change_percentage_24h"]);
                 this.setState({
                     name: data["id"],
                     symbol: data["name"],
                     price_usd: data["market_data"]["current_price"]["usd"],
                     price_mxn: data["market_data"]["current_price"]["mxn"],
                     market_cap_usd: data["market_data"]["market_cap"]["usd"],
+                    market_cap_rank: data["market_cap_rank"],
                     image: data["image"]["small"],
                     price_change_24h:
                         data["market_data"]["price_change_percentage_24h"],
@@ -34,6 +44,10 @@ class CardCoin extends Component {
                     high_24h: data["market_data"]["high_24h"]["usd"],
                 });
             });
+    }
+
+    async componentDidMount() {
+        this.fetchData();
     }
 
     render() {
@@ -62,16 +76,28 @@ class CardCoin extends Component {
                             </div>
                             <div className="col-md-12">
                                 <p className="text-uppercase text-center">
+                                    <strong>Market Cap Rank: </strong>
+                                    {this.state.market_cap_rank}
+                                </p>
+                            </div>
+                            <div className="col-md-12">
+                                <p className="text-uppercase text-center">
                                     <strong>Market Cap: </strong>
                                     {this.state.market_cap_usd} USD
                                     <br />
                                     <strong>Price change 24h: </strong>
                                     {this.state.price_change_24h > 0 ? (
                                         <span className="text-success">
-                                            {this.state.price_change_24h}
+                                            <FontAwesomeIcon
+                                                icon={faAngleDoubleUp}
+                                            />{" "}
+                                            {this.state.price_change_24h} %
                                         </span>
                                     ) : (
                                         <span className="text-danger">
+                                            <FontAwesomeIcon
+                                                icon={faAngleDoubleDown}
+                                            />{" "}
                                             {this.state.price_change_24h}
                                         </span>
                                     )}
@@ -97,6 +123,16 @@ class CardCoin extends Component {
                                 <p className="float-right">
                                     {this.state.price_mxn} MXN
                                 </p>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col-md-12">
+                                <button
+                                    className="btn btn-primary float-right rounded shadow"
+                                    onClick={() => this.fetchData()}
+                                >
+                                    <FontAwesomeIcon icon={faSync} />
+                                </button>
                             </div>
                         </div>
                     </CardBody>
