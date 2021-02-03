4c0220a0a9a011f3470d0d046d09632d5aa76bd6
ALXSMORA\Alexis Mora <alexis.mora1@icloud.com>
2021-02-02 22:28:13 -0600

    Add React Router Dom Add hashrouter. Prettier Code Style.

--- src/App.js
+++ src/App.js
@@ -2,14 +2,22 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "shards-ui/dist/css/shards.min.css";
 import "./App.css";
 
+import { Switch, Route, HashRouter } from "react-router-dom";
+
 import Layout from "./layout/Layout";
 
 function App() {
-  return (
-    <div>
-      <Layout />
-    </div>
-  );
+    return (
+        <HashRouter>
+            <div>
+                <Switch>
+                    <Route exact path="/">
+                        <Layout />
+                    </Route>
+                </Switch>
+            </div>
+        </HashRouter>
+    );
 }
 
 export default App;
